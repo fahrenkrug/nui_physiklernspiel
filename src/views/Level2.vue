01321bc82067969be0b03796b2fe1590b0097498
sarah <s.scheer@student.uni-luebeck.de>
2021-01-07 16:31:06 +0100

    added more logic with still a few bugs

--- src/views/Level2.vue
+++ src/views/Level2.vue
@@ -16,8 +16,9 @@ import {
   MouseConstraint,
   Mouse,
   World,
-  Body,
-  Bodies
+  //Body,
+  Bodies,
+  Events
 } from "matter-js";
 import LevelNavigation from "@/components/LevelNavigation";
 export default {
@@ -48,7 +49,7 @@ export default {
       this.setupEngine();
       this.setupWorld();
       this.setupMouse();
-      //this.listenForCollisionEvents();
+      this.listenForCollisionEvents();
     },
     setupEngine() {
       this.engine = Engine.create();
@@ -70,54 +71,36 @@ export default {
       Runner.run(this.runner, this.engine);
     },
     setupWorld() {
-      const group = Body.nextGroup(true);
+      //const group = Body.nextGroup(true);
+      //const group2 = Body.nextGroup(true);
       World.add(this.world, [
         // falling blocks
 
-        Bodies.rectangle(-200, 100, 100, 100, {
+        Bodies.rectangle(-400, 100, 100, 100, {
           collisionFilter: {
-            group: group
+            //group: group
           },
-          //isSensor: true,
+          isStatic: true,
           inertia: Infinity,
           render: {
             fillStyle: "red"
           }
         }),
-        Bodies.rectangle(0, 100, 100, 100, {
+        Bodies.rectangle(820, 100, 100, 100, {
           collisionFilter: {
-            group: group
+            //group: group
           },
-          //isSensor: true,
+          isStatic: true,
           inertia: Infinity,
           render: {
             fillStyle: "blue"
           }
         }),
-        Bodies.rectangle(200, 100, 100, 100, {
+        Bodies.rectangle(-400, 500, 100, 100, {
           collisionFilter: {
-            group: group
+            //group: group
           },
-          inertia: Infinity,
-          render: {
-            fillStyle: "green"
-          }
-        }),
-        Bodies.rectangle(400, 100, 100, 100, {
-          collisionFilter: {
-            group: group
-          },
-          //isSensor: true,
-          inertia: Infinity,
-          render: {
-            fillStyle: "yellow"
-          }
-        }),
-        Bodies.rectangle(-200, 500, 100, 100, {
-          collisionFilter: {
-            group: group
-          },
-          //isSensor: true,
+          isStatic: true,
           inertia: Infinity,
           render: {
             fillStyle: "white"
@@ -125,59 +108,77 @@ export default {
         }),
         Bodies.rectangle(0, 500, 100, 100, {
           collisionFilter: {
-            group: group
+            //group: group
           },
           //isSensor: true,
           inertia: Infinity,
           render: {
-            fillStyle: "black"
+            fillStyle: "white"
           }
         }),
         Bodies.rectangle(200, 500, 100, 100, {
           collisionFilter: {
-            group: group
+            //group: group
           },
           //isSensor: true,
           inertia: Infinity,
           render: {
-            fillStyle: "purple"
+            fillStyle: "white"
           }
         }),
-        Bodies.rectangle(400, 500, 100, 100, {
+        Bodies.rectangle(820, 500, 100, 100, {
           collisionFilter: {
-            group: group
+            //group: group
           },
-          //isSensor: true,
+          isStatic: true,
           inertia: Infinity,
           render: {
             fillStyle: "yellow"
           }
         }),
-        Bodies.rectangle(600, 100, 100, 100, {
+        Bodies.rectangle(400, 500, 100, 100, {
           collisionFilter: {
-            group: group
+            //group: group
           },
           //isSensor: true,
           inertia: Infinity,
           render: {
-            fillStyle: "blue"
-          }
-        }),
-        Bodies.rectangle(600, 500, 100, 100, {
-          collisionFilter: {
-            group: group
-          },
-          //isSensor: true,
-          inertia: Infinity,
-          render: {
-            fillStyle: "red"
+            fillStyle: "white"
           }
         }),
         // walls
-        Bodies.rectangle(220, 600, 1600, 50, { isStatic: true }),
-        Bodies.rectangle(-550, 100, 50, 1000, { isStatic: true }),
-        Bodies.rectangle(970, 100, 50, 1000, { isStatic: true }),
-        Bodies.rectangle(220, 0, 1600, 50, { isStatic: true })
+        Bodies.rectangle(220, 600, 1600, 50, {
+          collisionFilter: {
+            //group: group2
+          },render: {
+            fillStyle: "azure"
+          },
+          isStatic: true
+        }),
+        Bodies.rectangle(-550, 100, 50, 1000, {
+          collisionFilter: {
+            //group: group2
+          },render: {
+            fillStyle: "azure"
+          },
+          isStatic: true
+        }),
+        Bodies.rectangle(970, 100, 50, 1000, {
+          collisionFilter: {
+            //group: group2
+          },render: {
+            fillStyle: "azure"
+          },
+          isStatic: true
+        }),
+        Bodies.rectangle(220, 0, 1600, 50, {
+          collisionFilter: {
+            //group: group2
+          },render: {
+            fillStyle: "azure"
+          },
+          isStatic: true
+        })
       ]);
     },
     setupMouse() {
@@ -193,14 +194,64 @@ export default {
         });
       World.add(this.world, mouseConstraint);
 
-    // keep the mouse in sync with rendering
-    this.render.mouse = mouse;
+      // keep the mouse in sync with rendering
+      this.render.mouse = mouse;
 
-    // fit the render viewport to the scene
-    Render.lookAt(this.render, {
+      // fit the render viewport to the scene
+      Render.lookAt(this.render, {
         min: { x: 0, y: 0 },
         max: { x: 800, y: 600 }
-    });
+      });
+    },
+    listenForCollisionEvents() {
+      Events.on(this.engine, "collisionStart", function(event) {
+        var pairs = event.pairs;
+        let a = pairs[0].bodyA;
+        let b = pairs[0].bodyB;
+        let c1 = a.render.fillStyle;
+        let c2 = b.render.fillStyle;
+        console.log(c1);
+        console.log(c2);
+        if(c1 == "red" && c2== "white"){
+          b.render.fillStyle = "red";
+        }
+        if(c1 == "blue" && c2== "white"){
+          b.render.fillStyle = "blue";
+        }
+        if(c1 == "yellow" && c2== "white"){
+          b.render.fillStyle = "yellow";
+        }
+        if(c1 == "white"){
+          b.render.fillStyle = "white";
+        }
+        if(c1 == "blue" && c2== "yellow"){
+          b.render.fillStyle = "green";
+        }
+        if(c1 == "blue" && c2== "red"){
+          b.render.fillStyle = "purple";
+        }
+        if(c1 == "yellow" && c2== "red"){
+          b.render.fillStyle = "orange";
+        }
+        if(c1 == "yellow" && c2== "blue"){
+          b.render.fillStyle = "green";
+        }
+        if(c1 == "red" && c2== "blue"){
+          b.render.fillStyle = "purple";
+        }
+        if(c1 == "red" && c2== "yellow"){
+          b.render.fillStyle = "orange";
+        }
+        if( c2 == "azure"){
+          b.render.fillStyle = "azure";
+        }
+        
+
+
+
+
+         // Events.off(this.engine, "collisionEnd", this.onCollisionEnd);
+        });
     }
   }
 };
